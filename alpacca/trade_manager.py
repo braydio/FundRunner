f434d4651ba29158990b20d8c81f03e07c193f92
braydio <chaffee.brayden@gmail.com>
2025-03-15 06:10:23 -0400

    2025-03-15 - trade_manager.py - v1.0.0

---
+++ alpacca/trade_manager.py
@@ -0,0 +1,19 @@
+# trade_manager.py
+from alpacca.api_client import AlpacaClient
+
+class TradeManager:
+    def __init__(self):
+        self.client = AlpacaClient()
+
+    def buy(self, symbol, qty, order_type='market', time_in_force='gtc'):
+        return self.client.submit_order(symbol, qty, 'buy', order_type, time_in_force)
+
+    def sell(self, symbol, qty, order_type='market', time_in_force='gtc'):
+        return self.client.submit_order(symbol, qty, 'sell', order_type, time_in_force)
+
+    def cancel_order(self, order_id):
+        return self.client.cancel_order(order_id)
+
+    def list_open_orders(self):
+        return self.client.list_orders(status='open')
+
