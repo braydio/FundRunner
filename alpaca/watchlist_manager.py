7e8174277d17f887273cad69cbedbbfd80769c4e
braydio <chaffee.brayden@gmail.com>
2025-03-21 06:46:53 -0400

    2025-03-21 - watchlist_manager.py - v1.0.0

---
+++ alpaca/watchlist_manager.py
@@ -0,0 +1,24 @@
+# watchlist_manager.py
+from api_client import AlpacaClient
+
+class WatchlistManager:
+    def __init__(self):
+        self.client = AlpacaClient()
+
+    def list_watchlists(self):
+        return self.client.list_watchlists()
+
+    def create_watchlist(self, watchlist_name, symbols):
+        return self.client.create_watchlist(watchlist_name, symbols)
+
+    def add_to_watchlist(self, watchlist_id, symbol):
+        return self.client.add_to_watchlist(watchlist_id, symbol)
+
+    def remove_from_watchlist(self, watchlist_id, symbol):
+        return self.client.remove_from_watchlist(watchlist_id, symbol)
+
+    def get_watchlist(self, watchlist_id):
+        return self.client.get_watchlist(watchlist_id)
+
+    def delete_watchlist(self, watchlist_id):
+        return self.client.delete_watchlist(watchlist_id)
