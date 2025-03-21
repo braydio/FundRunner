c2a3d55c966c41412b363b03f73c008ee2310c8c
braydio <chaffee.brayden@gmail.com>
2025-03-21 06:04:42 -0400

    2025-03-21 - portfolio_manager.py - v1.0.0

---
+++ alpaca/portfolio_manager.py
@@ -0,0 +1,17 @@
+
+# portfolio_manager.py
+from api_client import AlpacaClient
+
+class PortfolioManager:
+    def __init__(self):
+        self.client = AlpacaClient()
+
+    def view_account(self):
+        return self.client.get_account()
+
+    def view_positions(self):
+        return self.client.list_positions()
+
+    def view_position(self, symbol):
+        return self.client.get_position(symbol)
+
